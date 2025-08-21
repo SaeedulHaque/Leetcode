bda1c20d6f3775022d4f7c6cef61babf2ac9f0e9
Saeedul Haque <86925176+SaeedulHaque@users.noreply.github.com>
2025-08-21 12:11:16 +0600

    Time: 16 ms (60.6%), Space: 12.5 MB (12.86%) - LeetHub

---
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.py
@@ -0,0 +1,37 @@
+class Solution(object):
+    def mergeAlternately(self, word1, word2):
+        """
+        :type word1: str
+        :type word2: str
+        :rtype: str
+        """
+        w1 = len(word1)
+        w2 = len(word2)
+
+        p1 = 0
+        p2 = 0
+
+        new_word=[]
+
+        word = 1
+
+        while p1<w1 and p2<w2:
+            if word == 1:
+                new_word.append(word1[p1])
+                p1+= 1
+                word = 2
+            else:
+                new_word.append(word2[p2])
+                p2+= 1
+                word = 1
+        # now once depleted, the rest will be appended
+        while p1<w1:
+            new_word.append(word1[p1])
+            p1+= 1 
+        
+        while p2<w2:
+            new_word.append(word2[p2])
+            p2+= 1 
+
+        return ''.join(new_word)
+            
