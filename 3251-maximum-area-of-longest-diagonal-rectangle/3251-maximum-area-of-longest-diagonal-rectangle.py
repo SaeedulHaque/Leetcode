1b735f0cd11a4bea5666900ab1829a1a3390308f
Saeedul Haque <86925176+SaeedulHaque@users.noreply.github.com>
2025-08-26 09:25:37 +0600

    Time: 0 ms (100%), Space: 12.4 MB (54.24%) - LeetHub

--- 3251-maximum-area-of-longest-diagonal-rectangle/3251-maximum-area-of-longest-diagonal-rectangle.py
+++ 3251-maximum-area-of-longest-diagonal-rectangle/3251-maximum-area-of-longest-diagonal-rectangle.py
@@ -8,7 +8,7 @@ class Solution(object):
         max_diagonal = 0
         max_area = 0
         for i in range(len(dimensions)):
-            diagonal = dimensions[i][0]**2 + dimensions[i][1]**2
+            diagonal = math.sqrt(dimensions[i][0]**2 + dimensions[i][1]**2)
             area = dimensions[i][0] * dimensions[i][1]
 
             if diagonal > max_diagonal:
