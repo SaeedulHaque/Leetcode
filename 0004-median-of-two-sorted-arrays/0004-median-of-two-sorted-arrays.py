a5b9911a7654ec343b5efcbd8184daafea6c006d
Saeedul Haque <86925176+SaeedulHaque@users.noreply.github.com>
2025-08-26 09:45:11 +0600

    Time: 4 ms (50.66%), Space: 12.7 MB (49.65%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -8,9 +8,9 @@ class Solution(object):
         nums = nums1+nums2
         n=len(nums)
         nums.sort()
-        if n%2 == 1: #odd
+        if n%2 == 0: #even
+            med = (nums[n/2 - 1] + nums[n/2]) / 2.0            
+        else: #odd
             med = nums[n/2]
-        else: #even
-            med = (nums[n/2 - 1] + nums[n/2]) / 2.0
 
         return med
