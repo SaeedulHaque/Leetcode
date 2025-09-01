4adc5e08f97ba09520ddbc6cd1b310c027e2aed2
Saeedul Haque <86925176+SaeedulHaque@users.noreply.github.com>
2025-09-01 10:10:05 +0600

    Time: 2 ms (60.93%), Space: 19.9 MB (65.7%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -7,8 +7,7 @@ class Solution(object):
         n = len(s)
         l = 0
         r = n-1
-        while l<r:
-
+        while l<r:  #for i in range n/2      iterating over half is enough
             s[l],s[r] = s[r],s[l]
             # temp = s[l]
             # s[l] = s[r]
