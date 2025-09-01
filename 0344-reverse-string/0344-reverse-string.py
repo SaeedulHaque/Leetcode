c9ba2574120315bb2ef5fc676ff9d7317c19186d
Saeedul Haque <86925176+SaeedulHaque@users.noreply.github.com>
2025-09-01 10:07:48 +0600

    Time: 2 ms (60.93%), Space: 19.8 MB (65.7%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -7,14 +7,10 @@ class Solution(object):
         n = len(s)
         l = 0
         r = n-1
-    
-        for i in range (n/2):
-            if l==r:
-                return s
-            else:
-                temp = s[l]
-                s[l] = s[r]
-                s[r] = temp
-                l+= 1
-                r+= -1
+        while l<r:
+            temp = s[l]
+            s[l] = s[r]
+            s[r] = temp
+            l+= 1
+            r+= -1
 
