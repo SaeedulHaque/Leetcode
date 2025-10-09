3b9baa0cd9d6a16abb7b885038872b9e6cbe923d
Saeedul Haque <86925176+SaeedulHaque@users.noreply.github.com>
2025-10-09 17:04:56 +0600

    Time: 3 ms (60.87%), Space: 14.9 MB (99.59%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
@@ -14,7 +14,7 @@ class Solution(object):
             return 0
         left = self.maxDepth(root.left)
         right = self.maxDepth(root.right)
-        return 1+max(left,right)
+        return max(left,right)+1
 
 
     
